232fa4eeca82f88ee0f2dab296d6c407da3f7fb4
Reed Paul MacDonald <47288934+reedmacdonald@users.noreply.github.com>
2022-09-20 20:43:18 -0700

    Added more origins

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+    const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:8020', 'http://127.0.0.1:9000', 'http://localhost:3000', 'http://reedmacdonald.com','https://reedmacdonald.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
